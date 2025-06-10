5fcb18aa27339e030c273898a8693d43b0f3badd
mutantsandeep <sasusharma09@gmail.com>
2025-06-10 14:40:19 +0530

    Deploy from dist folder

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CareerPathExplorer from "./CareerPathExplorer";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CareerPathExplorer />
+  </React.StrictMode>
+);
